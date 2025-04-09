119e9a3c48c58fef472b178b2bce9b98efae3b52
shovon-b <68199357+shovon-b@users.noreply.github.com>
2025-04-09 14:27:29 -0700

    Alternative layer with stronger label intrgration

---
+++ layers_strong.py
@@ -0,0 +1,154 @@
+#imports
+import pdb
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from torch.nn.utils.parametrizations import weight_norm as wn
+from utils import *
+
+#network in network class: changes the channel size: inpt: image[B, dim_in, W,H] -> out: image[B, dim_out, W,H]
+class nin(nn.Module):
+    def __init__(self, dim_in, dim_out):
+        super(nin, self).__init__()
+        self.lin_a = wn(nn.Linear(dim_in, dim_out))
+        self.dim_out = dim_out
+
+    def forward(self, x):
+        og_x = x
+        # assumes pytorch ordering        
+        x = x.permute(0, 2, 3, 1)
+        shp = list(x.shape)
+        out = self.lin_a(x.contiguous().view(shp[0]*shp[1]*shp[2], shp[3]))
+        shp[-1] = self.dim_out
+        out = out.view(shp)
+        return out.permute(0, 3, 1, 2)
+
+class down_shifted_conv2d(nn.Module):
+    def __init__(self, num_filters_in, num_filters_out, filter_size=(2,3), stride=(1,1),
+                    shift_output_down=False, norm='weight_norm'):
+        super(down_shifted_conv2d, self).__init__()
+
+        assert norm in [None, 'batch_norm', 'weight_norm']
+        self.conv = nn.Conv2d(num_filters_in, num_filters_out, filter_size, stride)
+        self.shift_output_down = shift_output_down
+        self.norm = norm
+        self.pad  = nn.ZeroPad2d((int((filter_size[1] - 1) / 2), # pad left
+                                  int((filter_size[1] - 1) / 2), # pad right
+                                  filter_size[0] - 1,            # pad top
+                                  0) )                           # pad down
+
+        if norm == 'weight_norm':
+            self.conv = wn(self.conv)
+        elif norm == 'batch_norm':
+            self.bn = nn.BatchNorm2d(num_filters_out)
+
+        if shift_output_down :
+            self.down_shift = lambda x : down_shift(x, pad=nn.ZeroPad2d((0, 0, 1, 0)))
+
+    def forward(self, x):
+        x = self.pad(x)
+        x = self.conv(x)
+        x = self.bn(x) if self.norm == 'batch_norm' else x
+        return self.down_shift(x) if self.shift_output_down else x
+
+class down_right_shifted_conv2d(nn.Module):
+    def __init__(self, num_filters_in, num_filters_out, filter_size=(2,2), stride=(1,1),
+                    shift_output_right=False, norm='weight_norm'):
+        super(down_right_shifted_conv2d, self).__init__()
+
+        assert norm in [None, 'batch_norm', 'weight_norm']
+        self.pad = nn.ZeroPad2d((filter_size[1] - 1, 0, filter_size[0] - 1, 0))
+        self.conv = nn.Conv2d(num_filters_in, num_filters_out, filter_size, stride=stride)
+        self.shift_output_right = shift_output_right
+        self.norm = norm
+
+        if norm == 'weight_norm':
+            self.conv = wn(self.conv)
+        elif norm == 'batch_norm':
+            self.bn = nn.BatchNorm2d(num_filters_out)
+
+        if shift_output_right :
+            self.right_shift = lambda x : right_shift(x, pad=nn.ZeroPad2d((1, 0, 0, 0)))
+
+    def forward(self, x):
+        x = self.pad(x)
+        x = self.conv(x)
+        x = self.bn(x) if self.norm == 'batch_norm' else x
+        return self.right_shift(x) if self.shift_output_right else x
+
+class down_shifted_deconv2d(nn.Module):
+    def __init__(self, num_filters_in, num_filters_out, filter_size=(2,3), stride=(1,1)):
+        super(down_shifted_deconv2d, self).__init__()
+        self.deconv = wn(nn.ConvTranspose2d(num_filters_in, num_filters_out, filter_size, stride,
+                                            output_padding=1))
+        self.filter_size = filter_size
+        self.stride = stride
+
+    def forward(self, x):
+        x = self.deconv(x)
+        #xs = [int(y) for y in x.size()]
+        xs = list(x.shape)
+        return x[:, :, :(xs[2] - self.filter_size[0] + 1),
+                 int((self.filter_size[1] - 1) / 2):(xs[3] - int((self.filter_size[1] - 1) / 2))]
+
+class down_right_shifted_deconv2d(nn.Module):
+    def __init__(self, num_filters_in, num_filters_out, filter_size=(2,2), stride=(1,1),
+                    shift_output_right=False):
+        super(down_right_shifted_deconv2d, self).__init__()
+        self.deconv = wn(nn.ConvTranspose2d(num_filters_in, num_filters_out, filter_size,
+                                                stride, output_padding=1))
+        self.filter_size = filter_size
+        self.stride = stride
+
+    def forward(self, x):
+        x = self.deconv(x)
+        xs = list(x.shape)
+        x = x[:, :, :(xs[2] - self.filter_size[0] + 1):, :(xs[3] - self.filter_size[1] + 1)]
+        return x
+
+
+'''
+skip connection parameter : 0 = no skip connection
+                            1 = skip connection where skip input size === input size
+                            2 = skip connection where skip input size === 2 * input size
+'''
+class gated_resnet(nn.Module):
+    def __init__(self, num_filters, conv_op, nonlinearity=concat_elu, skip_connection=0, conditional=False, num_class=4):
+        super(gated_resnet, self).__init__()
+        self.num_filters=num_filters
+        self.skip_connection = skip_connection
+        self.nonlinearity = nonlinearity
+        self.conv_input = conv_op(2 * num_filters, num_filters) # *2 because concat elu doubles the channel size
+
+        if skip_connection != 0 :
+            self.nin_skip = nin(2 * skip_connection * num_filters, num_filters)
+
+        self.dropout = nn.Dropout2d(0.5)
+        self.conv_out = conv_op(2 * num_filters, 2 * num_filters)        
+        # Conditional setup
+        self.conditional = conditional
+        if self.conditional:
+            self.embedding1 = nn.Embedding(num_class, num_filters)
+            self.embedding2 = nn.Embedding(num_class, 2 * num_filters)
+        else:
+            self.embedding1 = None
+            self.embedding2 = None
+
+
+    def forward(self, og_x, labels=None, a=None):
+        if self.conditional:
+            assert labels is not None, "Labels required for conditional mode"
+            bias1 = self.embedding1(labels).view(-1, self.num_filters, 1, 1)
+            bias2 = self.embedding2(labels).view(-1, 2 * self.num_filters, 1, 1)
+            #print("conditional model running")
+        else:
+            bias = None
+        x = bias1 + self.conv_input(self.nonlinearity(og_x)) if self.conditional else self.conv_input(self.nonlinearity(og_x))        
+        if a is not None :
+            x += self.nin_skip(self.nonlinearity(a))                
+        x = self.nonlinearity(x)
+        x = self.dropout(x)
+        x = self.conv_out(x) + bias2 if self.conditional else self.conv_out(x)        
+        a, b = torch.chunk(x, 2, dim=1)        
+        c3 = a * F.sigmoid(b)    
+        return og_x + c3 
